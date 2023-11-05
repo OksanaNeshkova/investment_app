b1e997e003d95bbefdc387fa987c2c79a21e12b7
Oksana Neshkova <oksana.neskova@gmail.com>
2023-11-05 16:25:07 +0200

    Login test and deleted register DTO method and class

--- src/main/java/com/example/SecurityTransactions/controller/AuthController.java
+++ src/main/java/com/example/SecurityTransactions/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.example.SecurityTransactions.controller;
 
 import com.example.SecurityTransactions.dto.LoginDto;
-import com.example.SecurityTransactions.dto.RegisterDto;
 import com.example.SecurityTransactions.entity.Employee;
 import com.example.SecurityTransactions.repo.EmployeeRepository;
 import io.jsonwebtoken.Jwts;
@@ -14,13 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.web.bind.annotation.*;
-
-
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -38,36 +34,14 @@ public class AuthController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-        if (employeeRepository.existsByEmail(registerDto.getEmail())) {
-            return new ResponseEntity<>("Employee with this email already exists", HttpStatus.BAD_REQUEST);
-        }
-        Employee employee = new Employee();
-        employee.setFirstName(registerDto.getFirstName());
-        employee.setLastName(registerDto.getLastName());
-        employee.setPersonalCode(registerDto.getPersonalCode());
-        employee.setPhone(registerDto.getPhone());
-        employee.setEmail(registerDto.getEmail());
-        employee.setAddress(registerDto.getAddress());
-        employee.setPassword(passwordEncoder.encode(registerDto.getPassword()));
-        employee.setRole(registerDto.getRole());
-
-        employeeRepository.save(employee);
-        return new ResponseEntity<>("New employee registered successfully", HttpStatus.OK);
-    }
-
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
-        System.out.println(loginDto.getEmail());
-        System.out.println(loginDto.getPassword());
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(),loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwtToken = Jwts.builder()
                         .setSubject(loginDto.getEmail())
                                 .signWith(SignatureAlgorithm.HS512,secretKey)
                                         .compact();
-        System.out.println("Response sent successfully");
         return new ResponseEntity<>(jwtToken,HttpStatus.OK);
     }
 
